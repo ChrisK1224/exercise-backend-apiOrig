b076b5d5d52a8b44658eab0fd148e9c3c2203c78
Christopher Katsaros <chris.katsaros34@gmail.com>
2022-01-23 12:26:39 -0500

    Add Typiecode Posts Call and tested to make sure it works. Also added gitIgnore files I guess they weren't created for some reason.

--- FieldLevel/FieldLevel/Controllers/FieldLevelController.cs
+++ FieldLevel/FieldLevel/Controllers/FieldLevelController.cs
@@ -1,13 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FieldLevel.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using static FieldLevel.Misc.ClientVariables;
 
 namespace FieldLevel.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class FieldLevelController : Controller
     {
-        
+        private readonly IHttpClientFactory _httpClientFactory;
+        public FieldLevelController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        [Route("LatestPosts")]
+        public async Task<ActionResult<TypicodePost[]>> GetLatestPosts()
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient(Typicode.Client);
+                HttpResponseMessage result = await client.GetAsync(Typicode.Calls.Posts);
+                if (result.IsSuccessStatusCode)
+                {
+                    var jsonResult = result.Content.ReadAsStringAsync().Result;
+                    var allPosts = JsonSerializer.Deserialize<TypicodePost[]>(jsonResult);
+                    return Ok(allPosts);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
     }
 }
